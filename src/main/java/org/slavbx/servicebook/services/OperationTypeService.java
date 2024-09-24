2bc34184dacaae1f2d5e84d3ebf2500dafa42e5e
slavbx <slavbx@gmail.com>
2024-09-24 16:18:18 +0500

    Публикация начатого проекта. Закомменчен недописанный метод

--- src/main/java/org/slavbx/servicebook/services/OperationTypeService.java
+++ src/main/java/org/slavbx/servicebook/services/OperationTypeService.java
@@ -33,24 +33,24 @@ public class OperationTypeService {
         return operationTypeRepository.findAll();
     }
 
-    public void refreshAllTypeStatus() {
-        Integer currentMileage = maintenanceService.findCurrentMileage();
-        this.findAll().forEach(t ->
-//                operationService.findAll().stream()
-//                        .filter(o1 -> o1.getType().equals(t) && currentMileage < o1.getMaintenance().getMileage() + o1.getType().getResource())
-//                        .findFirst()
-//                        .ifPresent(o2 -> t.setStatus("осталось " + (currentMileage - o2.getMaintenance().getMileage() + o2.getType().getResource()) + "км"))
-                  operationService.findAll().stream()
-                          .filter(o1 -> o1.getType().equals(t))
-                          .sorted((o1, o2) -> o2.getMaintenance().getMileage() - o1.getMaintenance().getMileage())
-                          .findFirst()
-                          .ifPresent(o3 -> currentMileage < o3.getMaintenance().getMileage() + o3.getType().getResource() ?
-
-
-                          )
-
-        );
-    }
+//    public void refreshAllTypeStatus() {
+//        Integer currentMileage = maintenanceService.findCurrentMileage();
+//        this.findAll().forEach(t ->
+////                operationService.findAll().stream()
+////                        .filter(o1 -> o1.getType().equals(t) && currentMileage < o1.getMaintenance().getMileage() + o1.getType().getResource())
+////                        .findFirst()
+////                        .ifPresent(o2 -> t.setStatus("осталось " + (currentMileage - o2.getMaintenance().getMileage() + o2.getType().getResource()) + "км"))
+//                  operationService.findAll().stream()
+//                          .filter(o1 -> o1.getType().equals(t))
+//                          .sorted((o1, o2) -> o2.getMaintenance().getMileage() - o1.getMaintenance().getMileage())
+//                          .findFirst()
+//                          .ifPresent(o3 -> currentMileage < o3.getMaintenance().getMileage() + o3.getType().getResource() ?
+//
+//
+//                          )
+//
+//        );
+//    }
 
 
 }
